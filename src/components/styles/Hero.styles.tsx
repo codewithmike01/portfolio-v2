99ea11615e686ef5b72c1a5fafb6ef2d662b095d
ginohmk <michotall95@gmail.com>
2023-01-03 05:44:35 +0100

    feat: Add hero section and font family

--- src/components/styles/Hero.styles.tsx
+++ src/components/styles/Hero.styles.tsx
@@ -2,13 +2,16 @@ import styled from 'styled-components';
 
 export const HeroContainer = styled.section`
   align-self: center;
-
+  width: 90%;
+  max-width: 1440px;
+  margin: 0 auto;
   .hero_wrapper {
     text-align: center;
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
     p {
+      align-self: center;
       max-width: 467px;
       line-height: 1.5rem;
       word-spacing: 1px;
