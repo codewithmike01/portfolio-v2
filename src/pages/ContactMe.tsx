13a2ff8667dac488408a43bed7c2611f7ba32316
ginohmk <michotall95@gmail.com>
2023-01-05 18:07:05 +0100

    feat: Add contact page

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+import NavBar from '../components/NavBar';
+import { ContactContainer } from './styles/ContactMe.styles';
 
 const ContactMe = () => {
-  return <div>ContactMe</div>;
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <NavBar />
+      <ContactContainer>
+        <h2>Contact Me</h2>
+
+        <form className="form_contact" onSubmit={(e) => handleSubmit(e)}>
+          <div className="input_field_container">
+            <TextField
+              label="Name"
+              placeholder="John Deo"
+              className="input_field"
+              required
+            />
+          </div>
+          <div className="input_field_container">
+            <TextField
+              label="Email"
+              placeholder="Myemail@gamil.com"
+              className="input_field"
+              required
+            />
+          </div>
+          <div className="input_field_container">
+            <textarea
+              placeholder="Type message..."
+              className="textarea"
+              required
+            />
+          </div>
+
+          <div className="button_container">
+            <Button variant="contained" type="submit">
+              Send
+            </Button>
+          </div>
+        </form>
+      </ContactContainer>
+    </>
+  );
 };
 
 export default ContactMe;
+// error
+// id="outlined-error"
---
+++ src/pages/styles/ContactMe.styles.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+export const ContactContainer = styled.section`
+  /* margin-top: 8rem; */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 3rem;
+  height: 100vh;
+
+  .form_contact {
+    width: 80%;
+    max-width: 1440px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 45px;
+    .input_field_container {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .input_field {
+        width: 100%;
+        max-width: 750px;
+      }
+
+      label:active {
+        color: var(--primary-bg);
+      }
+
+      .textarea {
+        width: 100%;
+        max-width: 730px;
+        height: 150px;
+        padding: 10px;
+        border: 1px solid #bebdbd;
+        border-radius: 4px;
+        font-size: 1rem;
+
+        &:focus {
+          outline: 1px solid var(--primary-bg);
+        }
+      }
+    }
+
+    .button_container {
+      width: 80%;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      button {
+        width: 280px;
+        height: 100%;
+        background-color: var(--primary-bg);
+        font-size: 1.2rem;
+        border-radius: 8px;
+
+        &:focus {
+          outline: 2px solid var(--primary-bg);
+        }
+      }
+    }
+  }
+`;
