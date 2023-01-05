260f8a8b504673afba80da3ea61f732c2904781b
ginohmk <michotall95@gmail.com>
2023-01-05 09:56:04 +0100

    feat: Add border color to testimony profile image

--- src/components/styles/Footer.styles.tsx
+++ src/components/styles/Footer.styles.tsx
@@ -4,13 +4,15 @@ export const FooterContainer = styled.section`
   background-color: #000;
   width: 100%;
   padding: 27px 0 3px 0;
+
   .footer_wrapper {
     width: 90%;
     margin: 0 auto;
     color: #fff;
-
     display: flex;
     justify-content: space-between;
+    gap: 2rem;
+    flex-wrap: wrap;
     .footer_social_links {
       display: flex;
       gap: 1rem;
--- src/components/styles/TestimonyCard.style.tsx
+++ src/components/styles/TestimonyCard.style.tsx
@@ -16,6 +16,9 @@ export const TestimonyContainer = styled.section`
   .testimony_card_image {
     width: 100px;
     height: 100px;
+    border: 4px solid rgba(23, 74, 255, 0.6);
+    border-radius: 50%;
+
     img {
       width: 100%;
       height: 100%;
@@ -33,15 +36,14 @@ export const TestimonyContainer = styled.section`
 
     a {
       text-decoration: none;
-      color: #000;
+      color: var(--primary-text);
       svg {
-        font-size: 1.2rem;
+        font-size: 1.25rem;
       }
     }
   }
 
   .card_text {
-    font-weight: 500;
     font-size: 1rem;
     font-weight: 500;
     text-align: center;
