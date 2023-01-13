ffc4982c9ba1718eec18e99ede84889237c776e5
ginohmk <michotall95@gmail.com>
2023-01-13 09:51:25 +0100

    feat: Add project details 30%

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { ProjectPageDetailContainer } from './styles/ProjectPDetailPage.style';
 import NavBar from '../components/NavBar';
+import ProjectPicture from '../assets/images/test_project1.png';
+
 const ProjectDetailPage = () => {
   return (
     <>
       <NavBar />
-      <ProjectPageDetailContainer>ProjectDetailPage</ProjectPageDetailContainer>
+      <ProjectPageDetailContainer>
+        <div className="project_details_container">
+          <img src={ProjectPicture} alt="product" />
+        </div>
+
+        <h2 className="project_name">Apply For Me</h2>
+
+        <section className="project_details_content ">
+          <div className="project_detail_skill details_section">
+            <h4>Tools used: </h4>
+            <div className="project_details_tag">
+              {['Reactjs', 'Typescript', 'Jest', 'Material Ui'].map((val) => (
+                <p>{val}</p>
+              ))}
+            </div>
+          </div>
+        </section>
+      </ProjectPageDetailContainer>
     </>
   );
 };
--- src/pages/styles/ProjectPDetailPage.style.tsx
+++ src/pages/styles/ProjectPDetailPage.style.tsx
@@ -1,8 +1,54 @@
 import styled from 'styled-components';
 
 export const ProjectPageDetailContainer = styled.section`
-  margin-top: 8rem;
   width: 90%;
   max-width: 1440px;
   margin: 0 auto;
+  margin-top: 13rem;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  .project_details_container {
+    width: 90%;
+    height: 550px;
+    margin: 0 auto;
+    img {
+      width: 100%;
+      height: 100%;
+      image-rendering: crisp-edges;
+    }
+  }
+
+  .project_name {
+    text-align: center;
+  }
+
+  .project_details_content {
+    margin-top: 45px;
+    display: flex;
+    flex-direction: column;
+    gap: 35px;
+    .details_section {
+      display: flex;
+      flex-direction: column;
+      gap: 15px;
+    }
+    .project_detail_skill {
+      .project_details_tag {
+        display: flex;
+        gap: 15px;
+        padding-left: 3rem;
+        p {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          padding: 10px 15px;
+          background-color: var(--primary-bg);
+          color: #fff;
+          border-radius: 4px;
+        }
+      }
+    }
+  }
 `;
