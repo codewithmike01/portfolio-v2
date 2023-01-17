d5e2cb01df488a6918438cc73c9120793bf683d0
ginohmk <michotall95@gmail.com>
2023-01-17 13:12:15 +0100

    Fix: set svg size for contactme

--- src/pages/styles/ProjectPDetailPage.style.tsx
+++ src/pages/styles/ProjectPDetailPage.style.tsx
@@ -48,7 +48,7 @@ export const ProjectPageDetailContainer = styled.section`
           text-decoration: none;
 
           svg {
-            font-size: 1.5rem;
+            font-size: 1.3rem;
           }
         }
       }
