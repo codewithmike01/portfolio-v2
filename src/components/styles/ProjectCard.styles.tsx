a37bd7c2b8934da883ea34bb4ce27cbe022b9afd
ginohmk <michotall95@gmail.com>
2023-01-20 22:15:16 +0100

    fix: Add more border radius to project card for visibility

--- src/components/styles/ProjectCard.styles.tsx
+++ src/components/styles/ProjectCard.styles.tsx
@@ -6,7 +6,7 @@ export const ProjectCardContainer = styled.div`
   flex-direction: column;
   gap: 20px;
   background: #ffffff;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 4px;
 
   .project_image_stack_container {
