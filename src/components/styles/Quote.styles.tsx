be273b42d6ea4f2aeaf5901353ec8ca58a17c1a2
ginohmk <michotall95@gmail.com>
2023-01-20 22:04:17 +0100

    fix: Add more realitic look to qoute card

--- src/components/styles/Quote.styles.tsx
+++ src/components/styles/Quote.styles.tsx
@@ -4,7 +4,7 @@ export const QuoteContainer = styled.section`
   width: 70%;
   max-width: 800px;
   margin: 0 auto;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 20px 5px 30px 5px;
   display: flex;
   align-items: center;
@@ -12,6 +12,7 @@ export const QuoteContainer = styled.section`
   flex-direction: column;
   height: 257px;
   position: relative;
+  border-radius: 8px;
 
   p {
     text-align: center;
