ebe1a165a1631a42078114a14e673c89cd05af2d
ginohmk <michotall95@gmail.com>
2023-01-20 22:27:31 +0100

    feat: Add meta data to public folder

--- src/components/styles/Quote.styles.tsx
+++ src/components/styles/Quote.styles.tsx
@@ -4,7 +4,7 @@ export const QuoteContainer = styled.section`
   width: 70%;
   max-width: 800px;
   margin: 0 auto;
-  box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px 4px rgba(0, 0, 0, 0.15);
   padding: 20px 5px 30px 5px;
   display: flex;
   align-items: center;
