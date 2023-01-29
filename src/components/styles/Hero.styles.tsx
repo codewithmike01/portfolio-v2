615aaf94eca65bb54dc7d51e6cb113a3af332565
ginohmk <michotall95@gmail.com>
2023-01-29 20:06:54 +0100

    feat: Add animation to hero page

--- src/components/styles/Hero.styles.tsx
+++ src/components/styles/Hero.styles.tsx
@@ -9,6 +9,26 @@ export const HeroContainer = styled.section`
   gap: 48px;
   margin-top: 10rem;
 
+  h2 {
+    position: relative;
+    left: 100%;
+
+    animation: floatLeft 1s ease-in-out forwards;
+
+    @keyframes floatLeft {
+      0% {
+        left: 100%;
+      }
+
+      50% {
+        left: 50%;
+      }
+      100% {
+        left: 0%;
+      }
+    }
+  }
+
   .hero_wrapper {
     text-align: center;
     display: flex;
@@ -21,10 +41,45 @@ export const HeroContainer = styled.section`
       line-height: 1.65rem;
       letter-spacing: 1.5px;
       word-spacing: 1px;
+      transform: scale(0);
+      transition: transform 1s ease-in-out;
+
+      animation: scaleIn 1s ease-in-out forwards;
+
+      @keyframes scaleIn {
+        0% {
+          transform: scale(0);
+        }
+        50% {
+          transform: scale(0.5);
+        }
+        75% {
+          transform: scale(0.75);
+        }
+        100% {
+          transform: scale(1);
+        }
+      }
     }
   }
 
   button {
     align-self: center;
+    position: relative;
+    right: 100%;
+
+    animation: floatRight 1s ease-in-out forwards;
+
+    @keyframes floatRight {
+      0% {
+        right: 100%;
+      }
+      50% {
+        right: 50%;
+      }
+      100% {
+        right: 0%;
+      }
+    }
   }
 `;
